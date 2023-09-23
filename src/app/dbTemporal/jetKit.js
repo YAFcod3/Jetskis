00ac697c347dc7ae75ea4c943c0bc2f286c1e467
unknown <you@example.com>
2023-09-23 17:31:22 -0400

    carpetas yate

--- src/app/dbTemporal/jetKit.js
+++ src/app/dbTemporal/jetKit.js
@@ -3,7 +3,7 @@
     {
       "id": 1,
       "name": "Jet Ski Rocket 2000",
-      "image": "/featureItem.jpg",
+      "image": "/image/yacht/featureItem.jpg", 
       "pricePerHour": 75,
       "description": "El Jet Ski Rocket 2000 es ideal para los amantes de la velocidad. Alcanza una velocidad máxima de 70 mph y ofrece una experiencia emocionante en el agua.",
       "rating": 4.7,
@@ -18,7 +18,7 @@
     {
       "id": 2,
       "name": "AquaRider Supreme",
-      "image": "/featureItem0.jpg",
+      "image": "/image/yacht/featureItem0.jpg",
       "pricePerHour": 60,
       "description": "El AquaRider Supreme es un jet ski versátil con una gran capacidad de almacenamiento. Perfecto para explorar la costa y hacer un picnic en la playa.",
       "rating": 4.5,
@@ -33,7 +33,7 @@
     {
       "id": 3,
       "name": "WaveMaster X-Treme",
-      "image": "/featureItem3.jpg",
+      "image": "/image/yacht/featureItem3.jpg",
       "pricePerHour": 80,
       "description": "El WaveMaster X-Treme ofrece una experiencia de conducción suave y estable. Ideal para realizar acrobacias y maniobras en el agua.",
       "rating": 4.9,
@@ -48,7 +48,7 @@
     {
       "id": 4,
       "name": "OceanBlaster Pro",
-      "image": "/featureItem4.jpg",
+      "image": "/image/yacht/featureItem4.jpg",
       "pricePerHour": 70,
       "description": "El OceanBlaster Pro es perfecto para el surf de jet ski. Con su potente motor, puedes enfrentarte a olas grandes y emocionantes.",
       "rating": 4.6,
@@ -63,7 +63,7 @@
     {
       "id": 5,
       "name": "SurgePro 3000",
-      "image": "/featureItem5.jpg",
+      "image": "/image/yacht/featureItem5.jpg",
       "pricePerHour": 85,
       "description": "El SurgePro 3000 es un jet ski de alto rendimiento diseñado para la emoción pura. Experimenta la adrenalina al máximo con este jet ski.",
       "rating": 4.8,
@@ -78,7 +78,7 @@
     {
       "id": 6,
       "name": "SunSplash Turbo",
-      "image": "/featureItem6.jpg",
+      "image": "/image/yacht/featureItem6.jpg",
       "pricePerHour": 75,
       "description": "El Jet Ski Rocket 2000 es ideal para los amantes de la velocidad. Alcanza una velocidad máxima de 70 mph y ofrece una experiencia emocionante en el agua.",
       "rating": 4.7,
