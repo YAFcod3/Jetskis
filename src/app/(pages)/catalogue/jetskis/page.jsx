f676d20f6326cd19742077c17439fc0d6e69e48d
YAFcod3 <yasmaniarmas1995@gmail.com>
2023-09-24 01:28:22 -0400

    commit

--- src/app/(pages)/catalogue/jetskis/page.jsx
+++ src/app/(pages)/catalogue/jetskis/page.jsx
@@ -4,7 +4,7 @@ import AllJetSki from "../components/AllJetSki";
 function JetKits() {
   return (
     <>
-      <h2 className="font-bold text-center text-4xl pt-16">Jet Kits </h2>
+      <h2 className="font-bold text-center text-4xl pt-16">Jet Skis </h2>
 
       <div className="pt-18  pb-24">
         <AllJetSki />
